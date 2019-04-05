08fb16c3b0c56b9b54f8cccfa59a474566af08ec
samuelsonokoi@gmail.com <samuelsonokoi@gmail.com>
2019-04-05 11:25:28 +0100

    completed application

--- db/migrate/20190401154012_create_todo_lists.rb
+++ db/migrate/20190401154012_create_todo_lists.rb
@@ -8,3 +8,17 @@ class CreateTodoLists < ActiveRecord::Migration[5.2]
     end
   end
 end
+
+def change
+    create_table :publications do |t|
+      t.string :title
+      t.text :description
+      t.references :publication_type
+      t.integer :publisher_id
+      t.string :publisher_type
+      t.boolean :single_issue
+ 
+      t.timestamps
+    end
+    add_index :publications, :publication_type_id
+  end
